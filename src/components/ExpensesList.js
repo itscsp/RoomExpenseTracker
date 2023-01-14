4ed23f93049ca7c80666b5f8902009774bae96a7
Chethan S Poojary <chethanspoojary1@gmail.com>
2023-01-14 13:32:13 +0530

    Almost UI Completed

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -2,7 +2,124 @@ import React from 'react'
 
 const ExpensesList = () => {
   return (
-    <div>ExpensesList</div>
+    <div className='pt-[20px] px-[16px] md:pt-[30px] md:px-[26px] flex flex-col justify-between h-full'>
+      {/* Expenses list cotainer */}
+      <div className='h-[600px] md:h-[360px] overflow-auto rounded-[10px]'>
+
+        {/* Single Expenses  */}
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+        <div className='grid grid-cols-8 justify-between items-center bg-white rounded-[10px] p-[10px] mb-[20px] gap-[10px] md:gap-0'>
+          <div className='flex gap-[30px]  items-center  col-span-8 md:col-span-6'>
+            <div className='flex justify-center items-center text-[36px]  bg-gray rounded-[5px] w-[50px]'>31</div>
+            <p className='text-[#575757]'>Grocerry Lorem ipsum dolor amet</p>
+          </div>
+          <div className='md:border-l md:border-gray col-span-8 md:col-span-2 justify-self-end md:justify-self-auto self-stretch flex items-center  '>
+           <p className='md:pl-[18px] text-black text-[20px] '>₹750.00</p>
+
+          </div>
+        </div>
+
+
+
+      </div>
+
+      {/* Expense total container */}
+      <div className='grid grid-cols-8  justify-between items-center h-[54px] mb-[10px] gap-[10px] md:gap-0'>
+        <h3 className='col-span-5 md:col-span-6 text-[20px]'>Total Expense </h3>
+        <div className='md:border-l md:border-white col-span-3 md:col-span-2 justify-self-center md:justify-self-auto self-stretch flex items-center'>
+         <p className='md:pl-[18px] text-black text-[20px]  font-semibold'>₹750.00</p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
