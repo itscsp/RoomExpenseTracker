df393755ecfd465f4f990f2029af4f82d1d94b52
Chethan S Poojary <chethanspoojary1@gmail.com>
2023-01-15 20:48:30 +0530

    Login screen changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ function LoginScreen(props) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState({});
-    const [validate, setValidate] = useState(false);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,24 +15,24 @@ function LoginScreen(props) {
             formErrors.email = 'Email is required';
         } else if (!/\S+@\S+\.\S+/.test(email)) {
             formErrors.email = 'Email address is invalid';
-            setValidate(true);
+         
 
         }else{
-            setValidate(true);
+   
         }
 
         if (!password) {
             formErrors.password = 'Password is required';
-            setValidate(false);
+          
         } else if (password.length < 8) {
             formErrors.password = 'Password must be at least 8 characters long';
-            setValidate(false);
+            
         } else {
             formErrors.password = '';
-            setValidate(true);
+            
         }
 
-        if(validate) {
+        if(!errors.email && !errors.password) {
             props.onLogin(email, password);
         }
         
@@ -57,8 +57,8 @@ function LoginScreen(props) {
             <div className=" bg-white rounded-lg m-4 p-[25px] shadow-xl">
 
                 <form onSubmit={handleSubmit} className="flex flex-col gap-[10px]">
-                    <h1 className='text-center font-semibold text-[42px]   text-black'>Expenses Tracker</h1>
-                    <h3 className='text-center font-semibold   md:text-[24px]  text-black'>Login</h3>
+                    <h1 className='text-center font-semibold text-[24px] md:text-[42px]   text-black'>Expenses Tracker</h1>
+                    <h3 className='text-center font-semibold   md:text-[18px]  text-black'>Login</h3>
                     <div>
 
                         <input
